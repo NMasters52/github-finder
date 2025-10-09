2386c276d86d7b8dad21c34108a96e0a028e507f
nmasters <nmasters52@gmail.com>
2025-10-09 10:15:08 -0400

    feat(api call) github user api fetch using react query
    - Use html form to search for specific user

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -5,8 +5,30 @@ const UserSearch = () => {
   const [username, setUsername] = useState("");
   const [submittedUsername, setSubmittedUsername] = useState("");
 
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["users", submittedUsername],
+    queryFn: async () => {
+      const res = await fetch(
+        `${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`
+      );
+
+      if (!res.ok) throw new Error("User not found");
+
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+    enabled: !!submittedUsername,
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setSubmittedUsername(username.trim());
+  };
+
   return (
-    <form className="form">
+    <form onSubmit={handleSubmit} className="form">
       <input
         type="text"
         value={username}
